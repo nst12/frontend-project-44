c3da9ead7600773a80866b4349db53d66f1bed1c
Anastasia S <reginald.fon.ravenhorst@yandex.ru>
2024-05-06 22:58:50 +0300

    Step4 fix variable type

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from "readline-sync"
 
 const greeting = () => {
-  let username = readlineSync.question("May I have your name? ");
+  const username = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${username}!`);
 };
 
