77ae582d9e2496b536a8f3e823ff3a1e64a69fd2
Anastasia <reginald.von.ravenhorst01@yandex.ru>
2024-05-09 13:25:55 +0300

    step 5 add brain-even game

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ import readlineSync from "readline-sync"
 const greeting = () => {
   const username = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${username}!`);
+  return username;
 };
 
 export { greeting };
