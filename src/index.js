3434cbcb47531b1ac89caaa053ca5b34e8ce45f7
Anastasia S <reginald.fon.ravenhorst@yandex.ru>
2024-05-23 22:50:11 +0300

    revert brain-games.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+export const greeting = () => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
   return username;
