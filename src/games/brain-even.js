b39c4aabd0aab7c86a45027a7a655a73e3e17b29
Anastasia S <reginald.fon.ravenhorst@yandex.ru>
2024-05-23 23:06:40 +0300

    fix eslint errors

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,12 +1,10 @@
 import { checkAnswer, getRandomNumber, startGame } from '../index.js';
 
-const isEven = (number) => {
-  return number % 2 === 0;
-};
+const isEven = (number) => number % 2 === 0;
 
 const getAttempt = () => {
   // Допустим, диапазон чисел от 0 до 99
-  let number = getRandomNumber(99);
+  const number = getRandomNumber(99);
   const isEvenNumber = isEven(number);
 
   const correctResult = isEvenNumber ? 'yes' : 'no';
